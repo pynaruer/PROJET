acc5806fe534d9f218e029b3334fadc172252c31
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-06-18 15:37:50 +0200

    18/06 14h

---
+++ Calcul_Final.py
@@ -0,0 +1,175 @@
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+
+def dicho(FP,bateau):
+    zga = -5
+    zgb = 0
+    epsilon = 0.01
+    zgmactuel = 0
+    listreturn = []
+    xreturn = []
+    index = 0
+    while True:
+        ancienzgm = zgmactuel
+        zgmactuel = (zga+zgb) / 2
+        resultat = CalculPousseeArchimede(bateau,zgmactuel - ancienzgm)
+        phi = resultat - FP
+        if abs(phi) < epsilon:
+            return -zga,listreturn,xreturn
+        elif phi >= 0:
+            zga = zgmactuel
+            listreturn.append(zga)
+        else:
+            zgb = zgmactuel
+            listreturn.append(zgb)
+        xreturn.append(index)
+        index +=1
+#Calcul du vecteur DS d'une facette à partir de AB et AC
+def CalculDS(A,B):
+    resultat = []
+    for i in range(3):
+        resultat.append((A[(i+1) % 3]*B[(i+2) % 3] - A[(i+2) % 3]*B[(i+1) % 3]) /2)
+    return resultat
+#Calcul de la force de pression sur une facette imergée
+def CalculF(Coordonee): #
+    rho = 1025
+    g = 9.81
+    #calcul des vecteurs AB et AC
+    AB = [Coordonee[1][0]-Coordonee[0][0], Coordonee[1][1]-Coordonee[0][1], Coordonee[1][2]-Coordonee[0][2]]
+    AC = [Coordonee[2][0]-Coordonee[0][0], Coordonee[2][1]-Coordonee[0][1], Coordonee[2][2]-Coordonee[0][2]]
+    Z = float((Coordonee[0][2]+Coordonee[1][2]+Coordonee[2][2])/3)
+    F = CalculDS(AB,AC) #m^2
+    pression_pascal = rho*g*Z
+    for i in range (len(F)):
+        F[i] *= pression_pascal #N
+    return F
+#Fonction pour trouver a partir des deux points le point sur la droite qui a Z = 0
+def PointAuNiveauDeLeauSurLaDroite(A,B):
+    AB = [B[0]-A[0],B[1]-A[1],B[2]-A[2]]
+    t = float(-A[2]/AB[2])
+    PointEnZ0 = [A[0]+AB[0]*t, A[1]+AB[1]*t, A[2]+AB[2]*t]
+    return PointEnZ0
+def CalculPousseeArchimede(bateau,Translation):
+    PousseArchimede = [0,0,0]
+    #On effectue un translation de X m
+    bateau.translaZ(Translation)
+    for i in bateau.getMesh().vectors:
+        if i[0][2] < 0 and i[1][2] < 0 and i[2][2] < 0 :
+            x=CalculF(i)
+            for n in range(len(PousseArchimede)) :
+                PousseArchimede[n] = PousseArchimede[n] + x[n]
+        elif i[0][2] < 0 or i[1][2] < 0 or i[2][2] < 0 : #cas particuliers
+    #Si A est sous l'eau a chaque fois
+            if i[0][2] < 0 and i[1][2] >= 0 and i[2][2] >= 0 :
+                ABen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[1])
+                ACen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[2])
+                NouvelleCoordonnee = [i[0],ABen0,ACen0]
+                x = CalculF(NouvelleCoordonnee)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n]
+            elif  i[0][2] < 0 and i[1][2] < 0 :
+                ACen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[2])
+                BCen0 = PointAuNiveauDeLeauSurLaDroite(i[1],i[2])
+                PremierDecoupage = [i[0],i[1],BCen0]
+                DeuxiemeDecoupage = [i[0],ACen0,BCen0]
+                x=CalculF(PremierDecoupage)
+                y=CalculF(DeuxiemeDecoupage)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n] + y[n]
+            elif i[0][2] < 0 and i[2][2] < 0 :
+                ABen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[1])
+                BCen0 = PointAuNiveauDeLeauSurLaDroite(i[1],i[2])
+                PremierDecoupage = [i[0],i[2],BCen0]
+                DeuxiemeDecoupage = [i[0],ABen0,BCen0]
+                x=CalculF(PremierDecoupage)
+                y=CalculF(DeuxiemeDecoupage)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n] + y[n]
+    #Si B est sous l'eau à chaque fois
+            if i[1][2] < 0 and i[0][2] >= 0 and i[2][2] >= 0 :
+                ABen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[1])
+                BCen0 = PointAuNiveauDeLeauSurLaDroite(i[1],i[2])
+                NouvelleCoordonnee = [i[1],ABen0,BCen0]
+                x = CalculF(NouvelleCoordonnee)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n]
+            elif  i[1][2] < 0 and i[0][2] < 0 :
+                ACen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[2])
+                BCen0 = PointAuNiveauDeLeauSurLaDroite(i[1],i[2])
+                PremierDecoupage = [i[0],i[1],ACen0]
+                DeuxiemeDecoupage = [i[0],ACen0,BCen0]
+                x=CalculF(PremierDecoupage)
+                y=CalculF(DeuxiemeDecoupage)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n] + y[n]
+            elif i[1][2] < 0 and i[2][2] < 0 :
+                ABen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[1])
+                ACen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[2])
+                PremierDecoupage = [i[1],i[2],ACen0]
+                DeuxiemeDecoupage = [i[1],ACen0,ABen0]
+                x=CalculF(PremierDecoupage)
+                y=CalculF(DeuxiemeDecoupage)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n] + y[n]
+    #Si C est sous l'eau à chaque fois
+            if i[2][2] < 0 and i[1][2] >= 0 and i[0][2] >= 0 :
+                BCen0 = PointAuNiveauDeLeauSurLaDroite(i[1],i[2])
+                CAen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[2])
+                NouvelleCoordonnee = [i[2],BCen0,CAen0]
+                x = CalculF(NouvelleCoordonnee)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n]
+            elif  i[2][2] < 0 and i[1][2] < 0 :
+                ACen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[2])
+                ABen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[1])
+                PremierDecoupage = [i[1],i[2],ABen0]
+                DeuxiemeDecoupage = [i[2],ACen0,ABen0]
+                x=CalculF(PremierDecoupage)
+                y=CalculF(DeuxiemeDecoupage)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n] + y[n]
+            elif i[2][2] < 0 and i[0][2] < 0 :
+                ABen0 = PointAuNiveauDeLeauSurLaDroite(i[0],i[1])
+                BCen0 = PointAuNiveauDeLeauSurLaDroite(i[1],i[2])
+                PremierDecoupage = [i[0],i[2],ABen0]
+                DeuxiemeDecoupage = [i[2],ABen0,BCen0]
+                x=CalculF(PremierDecoupage)
+                y=CalculF(DeuxiemeDecoupage)
+                for n in range(len(PousseArchimede)) :
+                    PousseArchimede[n] = PousseArchimede[n] + x[n] + y[n]
+    return PousseArchimede[2]
+class Bateau:
+    def __init__(self,profondeur,masse,coque,gravite):
+        self.__profondeur = profondeur
+        self.__masse = masse
+        self.__coque = coque
+        self.__gravite = gravite
+        self.__mesh = mesh.Mesh.from_file(self.__coque)
+    def getProfondeur(self): return self.__profondeur
+    def getPoids(self): return (self.__masse * 9.81)
+    def getCoque(self): return self.__coque
+    def getMesh(self): return self.__mesh
+    def getGravite(self): return self.__gravite
+    def translaZ(self,choixtransla):
+        for i in self.__mesh.vectors:
+            for y in i:
+                y[2] = y[2] + choixtransla
+#Tests
+#tonnerre = Bateau("PetitTonerre",100,'Rectangular_HULL.stl',9.81)
+#print(CalculF(tonnerre.getMesh().vectors[0],9.81))
+#print(CalculF([[0,0,0],[1,0,0],[0,1,0]],9.81))
+#print(CalculF([[0,0,-1],[1,0,-1],[0,1,-1]],9.81)) On s'attends a pression/2
+# Test calcul DS
+# a = [1,0,0]
+# b = [0,1,0]
+#print(CalculPousseeArchimede(tonnerre,0))
+# print(CalculDS(a,b))
+#print(CalculDS([-3,7,4],[1,8,3])) #test CalculDS : affiche théoriquement [-5.5, 6.5, -15.5]
+#print(CalculDS([-23,63,-8],[12,40,-44])) #2eme test de CalculDS : affiche théoriquement [-1226, -554, -838]
+#print(CalculF([[4,2,3],[1,9,7],[5,10,6]])) #test CalculF : affiche théoriquement [-294954, 348582, -831234]
+#print(CalculF([[20,-34,12],[-3,29,4],[32,6,-32]])) #2eme test de CalculF : affiche théoriquement [65747928, 29602656, 44940264]
+#print(PointAuNiveauDeLeauSurLaDroite([1,3,2],[-5,0,4])) #test PointAuNiveauDeLeauSurLaDroite : affiche théoriquement [7,6,0]
+#print(CalculPousseeArchimede(tonnerre,-1)) #test CalculPousseeArchimede : affiche théoriquement 80442 N
+FP=1590.8*9.81    #test dichotomie : affiche théoriquement 0,194
+#print(dicho(FP,tonnerre))  #suite du test
--- Interface.py
+++ Interface.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from mpl_toolkits import mplot3d
 from stl import mesh
+from Calcul_Final import *
+import numpy as np
+import time
 
 
 """Programme Interface Paramètres"""
@@ -13,9 +16,10 @@ class Entry_Box(QWidget):
         self.number = 0
         self.__boat = ""
         self.__gravite = 0
-        self.__profondeur = 0
         self.setWindowTitle("Position d'équilibre d'un bateau")
-        self.setFixedSize(600, 300)
+        self.setFixedSize(600, 400)
+        icon = QIcon("logo.png")
+        self.setWindowIcon(icon)
 
         #Layouts
         self.layout = QVBoxLayout()
@@ -24,6 +28,8 @@ class Entry_Box(QWidget):
         self.layoutEntrees = QHBoxLayout()
         self.layoutBateau1 = QHBoxLayout()
         self.layoutBateau2 = QHBoxLayout()
+        self.layoutBateau3 = QHBoxLayout()
+        self.layoutBateau4 = QHBoxLayout()
         self.specialLayout = QHBoxLayout()
 
         #Style
@@ -44,7 +50,7 @@ class Entry_Box(QWidget):
         self.layoutGrav.addWidget(self.choose)
 
         #Entree profondeur
-        self.entreeprof = QLabel("Veuillez saisir la profondeur de départ (en m) : ")
+        self.entreeprof = QLabel("Veuillez saisir la profondeur (en m) : ")
         self.prof = QDoubleSpinBox()
         self.prof.setMinimum(-999999999)
         self.prof.setMaximum(999999999)
@@ -53,7 +59,7 @@ class Entry_Box(QWidget):
         self.entreemasse = QLabel("Entrez la masse du bateau (en kg) : ")
         self.txtenter = QDoubleSpinBox()
         self.txtenter.setMaximum(999999999)
-        self.txtenter.setMinimum(0)
+        self.txtenter.setMinimum(1)
 
         #Organisation des layouts
         self.layoutEntrees.addWidget(self.entreemasse)
@@ -67,27 +73,40 @@ class Entry_Box(QWidget):
         self.layout.addLayout(self.layoutProf)
         self.layout.addWidget(self.choix2)
 
-        self.VHullButton = QRadioButton("   Bateau 1 : V_HULL")
+        self.VHullButton = QRadioButton("Bateau 1 : V_HULL")
         self.layoutBateau1.addWidget(self.VHullButton)
 
         self.RectangularButton = QRadioButton("Bateau 2 : Rectangular_HULL")
         self.layoutBateau1.addWidget(self.RectangularButton)
 
-        self.CylButton = QRadioButton("   Bateau 3 : Cylindrical_HULL")
+        self.CylButton = QRadioButton("Bateau 3 : Cylindrical_HULL")
         self.layoutBateau2.addWidget(self.CylButton)
 
         self.MiniButton = QRadioButton("Bateau 4 : Mini650_HULL")
         self.layoutBateau2.addWidget(self.MiniButton)
 
+        self.AluButton = QRadioButton("Bateau 5 : BargeAlu_HULL")
+        self.layoutBateau3.addWidget(self.AluButton)
+
+        self.ssMarinButton = QRadioButton("Bateau 6 : ssMarin_HULL")
+        self.layoutBateau3.addWidget(self.ssMarinButton)
+
+        self.WigleyButton = QRadioButton("Bateau 7 : Wigley_HULL")
+        self.layoutBateau4.addWidget(self.WigleyButton)
+
         self.newButton = QRadioButton()
         self.specialLayout.addWidget(self.newButton)
-
         self.txtButton = QLineEdit("Entrez le fichier de votre choix")
         self.specialLayout.addWidget(self.txtButton)
 
         self.layout.addLayout(self.layoutBateau1)
         self.layout.addLayout(self.layoutBateau2)
+        self.layout.addLayout(self.layoutBateau3)
+        self.layout.addLayout(self.layoutBateau4)
         self.layout.addLayout(self.specialLayout)
+
+        self.txtButton = QLineEdit("Entrez le fichier de votre choix")
+        self.specialLayout.addWidget(self.txtButton)
 
         self.buttonvalidate = QPushButton("Valider")
         self.buttonvalidate.clicked.connect(self.buttonClicked)
@@ -118,9 +137,6 @@ class Entry_Box(QWidget):
 
         #Recuperation de la masse
         self.__masse = self.txtenter.value()
-
-        #Recuperation de la profondeur
-        self.__profondeur = self.prof.value()
 
         #Recuperation de l'url de l'stl
         self.__boat = self.txtButton.text()
@@ -156,43 +172,70 @@ class Entry_Box(QWidget):
             self.__boat = str(self.txtButton.text())
             self.verification()
 
+        elif self.ssMarinButton.isChecked() == True:
+            self.layout.removeWidget(self.txtErreur)
+            self.__boat = "ssmarin_L=4_W=2_H=2_Normals_Outward.STL"
+            self.verification()
+
+        elif self.WigleyButton.isChecked() == True:
+            self.layout.removeWidget(self.txtErreur)
+            self.__boat = "WIGLEY_L=2500_B=250_T=156_NormalOutward2.STL"
+            self.verification()
+
+        elif self.AluButton.isChecked() == True:
+            self.layout.removeWidget(self.txtErreur)
+            self.__boat = "BargeAlu_L_2980_W_633_H_400_NormalOutward2.STL"
+            self.verification()
+
     def getBoat(self):
         return self.__boat
-
+    def getProfondeur(self):
+        return self.entreeprof
+    def getMasse(self):
+        return self.__masse
+    def getGravite(self):
+        return self.__gravite
     def getNumber(self):
         return self.number
 
-
 """Programme interface finale"""
 class Interface(QWidget):
-    def __init__(self,boat):
+    def __init__(self,boat,tirant,listgraph,listx):
         QWidget.__init__(self)
-        self.boat = boat
+        self.coque = boat.getCoque()
+        self.__tirant = tirant
+        self.__listgraph = listgraph
+        self.__listx = listx
         self.setWindowTitle("Interface Bateau")
         self.layout = QGridLayout()
-        self.setFixedSize(500,300)
-        icon = QIcon("bateau")
-        self.setWindowIcon(icon)
 
         #Style
         self.setStyleSheet(open('mystylesheet.css').read())
+        self.setFixedSize(600,400)
+        icon = QIcon("logo.png")
+        self.setWindowIcon(icon)
 
         #Button
         self.button_1 = QPushButton("Start simulation")
-        #self.button_1.setStyleSheet("background-image: url(logo.jpg)")
-        self.button_1.setFixedSize(450,273)
+        self.progressbar = QProgressBar()
 
         self.layout.addWidget(self.button_1)
+        self.layout.addWidget(self.progressbar)
+        self.progressbar.hide()
+
         self.button_1.clicked.connect(self.start_simulation)
 
         self.setLayout(self.layout)
 
     #Signaux
     def start_simulation(self):
-        self.setFixedSize(1000,500)
-
         self.button_1.hide()
-
+        self.progressbar.show()
+        for i in range(101):
+            self.progressbar.setValue(i)
+            time.sleep(0.05)
+        self.setFixedSize(1000,550)
+        self.progressbar.hide()
         self.button_2 = QPushButton("Exit")
         self.layout.addWidget(self.button_2,8,8,1,1)
         self.button_2.clicked.connect(self.exit)
@@ -200,7 +243,8 @@ class Interface(QWidget):
         #Graphique
         self.fig2 = plt.figure()
         self.graph = FigureCanvas(self.fig2)
-        plt.plot([1,2,3,4])
+        plt.ylim(-4,0)
+        plt.plot(self.__listx,self.__listgraph)
         plt.xlabel('Calcul de la position du tirant d\'eau')
         self.graph.draw()
         self.layout.addWidget(self.graph,2,5,4,4)
@@ -209,7 +253,7 @@ class Interface(QWidget):
         self.fig = plt.figure()
         self.canvas = FigureCanvas(self.fig)
         axes = mplot3d.Axes3D(self.fig)
-        your_mesh = mesh.Mesh.from_file(self.boat)
+        your_mesh = mesh.Mesh.from_file(self.coque)
         axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
         scale = your_mesh.points.flatten("C")
         axes.auto_scale_xyz(scale, scale, scale)
@@ -220,15 +264,16 @@ class Interface(QWidget):
         self.close()
 
 
-
 if __name__ == "__main__":
     import sys
     app = QApplication([])
     window_1 = Entry_Box()
     window_1.show()
     app.exec_()
+    tonnerre = Bateau(window_1.getProfondeur(),window_1.getMasse(),window_1.getBoat(),window_1.getGravite())
+    tirant,listgraph,listx = dicho(window_1.getMasse()*window_1.getGravite(),tonnerre)
     if window_1.getNumber() == 1:
-        window_2 = Interface(window_1.getBoat())
+        window_2 = Interface(tonnerre,tirant,listgraph,listx)
         window_2.show()
         app.exec_()
         sys.exit()
