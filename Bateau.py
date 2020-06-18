efe57143416e56e91504a3485d57669a1a96160d
pynaruer <62301358+pynaruer@users.noreply.github.com>
2020-06-18 09:22:27 +0200

    18/06 9h

--- Bateau.py
+++ Bateau.py
@@ -1,32 +1,33 @@
 from Algo_Dichotomie import *
 
 class Bateau:
-    def __init__(self,nom,masse,coque):
+    def __init__(self, nom, masse, boat, gravite):
         self.__nom = nom
         self.__masse = masse
-        self.__coque = coque
-        self.__mesh = mesh.Mesh.from_file(self.__coque)
-
+        self.__boat = boat
+        self.__gravite = gravite
+        self.__mesh = mesh.Mesh.from_file(self.__boat)
     def getNom(self):
         return self.__nom
-    def getPoids(self):
-        return (self.__masse * 9.81)
+    def getMasse(self):
+        return self.__masse
     def getCoque(self):
-        return self.__coque
+        return self.__boat
+    def getGravite(self):
+        return self.__gravite
     def getMesh(self):
         return self.__mesh
-
     def translaZ(self,choixtransla):
         for i in self.__mesh.vectors:
             for y in i:
                 y[2] = y[2] + choixtransla
 
 # #Tests
-# tonnerre = Bateau("PetitTonerre",100,'Cylindrical__HULL.stl')
-# print(CalculF(tonnerre.getMesh().vectors[0],9.81))
-# print(CalculF([[0,0,0],[1,0,0],[0,1,0]],9.81))
-# print(CalculF([[0,0,-1],[1,0,-1],[0,1,-1]],9.81)) #On s'attends a pression/2
-
+# tonnerre = Bateau("PetitTonerre",100,'Cylindrical_HULL.stl',9)
+# print(CalculF(tonnerre.getMesh().vectors[0]))
+# print(CalculF([[0,0,0],[1,0,0],[0,1,0]]))
+# print(CalculF([[0,0,-1],[1,0,-1],[0,1,-1]])) #On s'attends a pression/2
+#
 # #Test calcul DS
 # a = [1,0,0]
 # b = [0,1,0]
